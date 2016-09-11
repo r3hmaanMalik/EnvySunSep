b694f6799858aab33fdcd179761d57c7f7f31528
r3hmaanMalik <r3hmaan@hotmail.com>
2016-09-12 02:17:14 +0500

    done

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -2,4 +2,16 @@ class PicsController < ApplicationController
 	def index
 		
 	end
+	def new
+		@pic = Pic.new
+	end
+	def create
+		@pic = Pic.new(pic_params)
+	end
+
+	private
+
+	def pic_params
+		params.require(:pic).permit(:title, :discription)
+	end
 end
